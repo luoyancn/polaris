cb05e6391b35c0805252c6aafc5d642d8977320d
luoyancn <luoyan_cn@outlook.com>
2018-07-18 15:59:57 +0800

    Add grpc tls server/client examples

---
+++ src/example/grpctls/grpctlsclient/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/luoyancn/dubhe/common"
+	"github.com/luoyancn/dubhe/fake/msg"
+	"github.com/luoyancn/dubhe/grpclib"
+	"github.com/luoyancn/dubhe/grpclib/config"
+	"github.com/luoyancn/dubhe/logging"
+)
+
+func main() {
+	logging.GetLogger("test", logging.STD_ENABLED, "", true)
+	grpcfg := config.NewGrpConf()
+	common.ReadConfig(
+		"ssl.toml", "test", logging.STD_ENABLED, "", true, true, grpcfg)
+	ctx, cancle := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancle()
+	grpclib.InitGrpcClientPool("127.0.0.1:8080")
+	conn := grpclib.Get()
+	defer grpclib.Put(conn)
+	client := msg.NewMessagesClient(conn)
+	resp, err := client.Call(ctx, &msg.Request{Req: "luoyan"})
+	if nil != err {
+		fmt.Printf("Failed to get response from grpc server:%v\n", err)
+		return
+	}
+	fmt.Printf("%s\n", resp.GetResp())
+}
--- src/example/grpctls/grpctlserver/main.go
+++ src/example/grpctls/grpctlserver/main.go
@@ -4,13 +4,18 @@ import (
 	"github.com/luoyancn/dubhe/common"
 	"github.com/luoyancn/dubhe/fake/msg"
 	"github.com/luoyancn/dubhe/grpclib"
+	"github.com/luoyancn/dubhe/grpclib/config"
 	"github.com/luoyancn/dubhe/logging"
 )
 
 func main() {
 	logging.GetLogger("test", logging.STD_ENABLED, "", true)
+	grpcfg := config.NewGrpConf()
+	common.ReadConfig(
+		"ssl.toml", "test", logging.STD_ENABLED, "", true, true, grpcfg)
 	go grpclib.StartServer(
-		8080, &msg.Service{HostName: "zhangjl", ListenPort: 8080},
+		config.GRPC_PORT, &msg.Service{HostName: "zhangjl",
+			ListenPort: config.GRPC_PORT},
 		msg.Messages_serviceDesc)
 	common.Wait(grpclib.StopServer)
 }
