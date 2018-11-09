c1c990326e1d4d2fc5639ea11eeda53769f480ba
luoyancn <luoyan_cn@outlook.com>
2018-11-09 09:34:57 +0800

    Fix the bug of example

--- src/example/oceanstack/main.go
+++ src/example/oceanstack/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/luoyancn/dubhe/common"
 	"github.com/luoyancn/dubhe/logging"
+	"github.com/luoyancn/merak"
 
 	"github.com/luoyancn/ocean/config"
 	"github.com/luoyancn/ocean/keystone"
@@ -10,11 +11,19 @@ import (
 )
 
 func main() {
+
+	merak.Exec([]string{"ps", "-efl", "|", "grep", "hugo"})
 	cfg := config.NewRdoConf()
 	common.ReadConfig("rdo.toml", "rdo", logging.FILE_ENABLED,
-		"rdo", true, true, cfg)
-	ctx, token, _ := keystone.Authorization()
+		"/tmp", true, true, cfg)
+	ctx, token, err := keystone.Authorization()
+	if nil != err {
+		return
+	}
 	logging.LOG.Debugf("The auth token is: %s\n", token)
-	nova.ForceDownAndComputeSrv(ctx, token, "kubernetes3", true)
+	err = nova.ForceDownAndComputeSrv(ctx, token, "kubernetes3", true)
+	if nil != err {
+		return
+	}
 	nova.EvacuateVmOnHost(ctx, token, "kubernetes3")
 }
