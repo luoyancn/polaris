40e4803a2ed1569623802577dff972b725ad1547
luoyancn <luoyan_cn@outlook.com>
2018-11-23 12:14:12 +0800

    Update the example

--- src/example/oceanstack/main.go
+++ src/example/oceanstack/main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	pipes "github.com/ebuchman/go-shell-pipes"
+
 	"github.com/luoyancn/dubhe/common"
 	"github.com/luoyancn/dubhe/logging"
-	"github.com/luoyancn/merak"
 
 	"github.com/luoyancn/ocean/config"
 	"github.com/luoyancn/ocean/keystone"
@@ -12,14 +13,17 @@ import (
 
 func main() {
 
-	merak.Exec([]string{"ps", "-efl", "|", "grep", "hugo"})
 	cfg := config.NewRdoConf()
 	common.ReadConfig("rdo.toml", "rdo", logging.FILE_ENABLED,
 		"/tmp", true, true, cfg)
+	output, err := pipes.RunString(`cat /tmp/rdo | awk '{print $1}'| grep 2012`)
+	logging.LOG.Infof("Shell cmd result is %s\n", output)
+
 	ctx, token, err := keystone.Authorization()
 	if nil != err {
 		return
 	}
+
 	logging.LOG.Debugf("The auth token is: %s\n", token)
 	err = nova.ForceDownAndComputeSrv(ctx, token, "kubernetes3", true)
 	if nil != err {
